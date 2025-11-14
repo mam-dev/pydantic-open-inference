5fb4616ca422e245a0bdf1d86b43684f6feac491
bunny-therapist <87039365+bunny-therapist@users.noreply.github.com>
2025-11-14 17:07:55 +0100

    Add provisional publish job

---
+++ tests/smoke_test.py
@@ -0,0 +1,29 @@
+"""Smoke tests to run on wheel/sdist before publishing.
+
+Just checks that imports etc work properly.
+
+"""
+from pydantic_open_inference import (
+    BadStatusCodeFromServerError,  # noqa: F401
+    DatatypeOverride,  # noqa: F401
+    InputsBaseModel,
+    OutputsBaseModel,
+    PydanticOpenInferenceError,  # noqa: F401
+    RemoteModel,
+)
+
+
+class SmokeTestInputs(InputsBaseModel):
+    dummy: str
+
+
+class SmokeTestOutputs(OutputsBaseModel):
+    dummy: str
+
+
+_ = RemoteModel(
+    model_name="dummy",
+    inputs_model=SmokeTestInputs,
+    outputs_model=OutputsBaseModel,
+    server_url="http://localhost:8080",
+)
